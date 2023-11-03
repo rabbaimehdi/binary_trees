92eec713de699fe3048319e5823c8c6095d12725
Mehdi <mehdi.rabbai@gmail.com>
2023-11-02 17:10:44 -0700

    update 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,34 +13,3 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	return (binary_tree_size(tree) - binary_tree_leaves(tree));
 }
-
-/**
- * binary_tree_size - the size of a binary tree
- * @tree: tree to measure the size of
- *
- * Return: size of the tree 0 if tree is NULL
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
-}
-
-/**
- * binary_tree_leaves - the leaves in a binary tree
- * @tree: tree to count the leaves from
- *
- * Return: number of leaves 0 if tree is NULL
- */
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-
-	if (!tree->left && !tree->right)
-		return (1);
-
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-}
